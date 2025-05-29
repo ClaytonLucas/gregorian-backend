c9536f819d4949ab2181d8515bdc5712839aa833
ClaytonLucas <clayton.lucas07@gmail.com>
2025-05-29 00:02:12 -0300

    third commit

--- models/index.js
+++ models/index.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // necessário para Render
+    },
+  },
 });
 
 module.exports = sequelize;
